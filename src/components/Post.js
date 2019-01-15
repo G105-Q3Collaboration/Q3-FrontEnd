38e604ed7aa897e6dd8a4eb02816738390af7d1e
Katrina Agustin <katrina.agustin@gmail.com>
2019-01-15 12:04:09 -0800

    remove fn on fn comp

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { FaThumbsUp, FaPoop, FaSadCry, FaGrinSquint, FaTrash, FaHeart } from 'react-icons/fa'
 import Moment from 'react-moment'
 
-const Post = ({ username, content, getPosts, createdAt}) => {
+const Post = ({ username, content, createdAt}) => {
     return (
-      <div className="post border rounded p-0 mt-4" onLoad={() => getPosts()}>
+      <div className="post border rounded p-0 mt-4">
         <div className="pl-3 controls d-flex justify-content-between align-items-center">
           <p className="thumb mt-3">{username} shared a post:<br/>
             <small><Moment format="MMM D, YYYY" date={createdAt} /></small>
@@ -14,7 +14,7 @@ const Post = ({ username, content, getPosts, createdAt}) => {
         <p className="lead pl-3 pr-3">{content}</p>
         {/* <img className="card-img-top" src="https://watermarked.cutcaster.com/cutcaster-photo-100067117-Cat-side-profile.jpg" alt="Cat"/> */}
           <div className="reactions p-2">
-            <span className="reaction mr-1" onClick={() => this.props.getPosts}><FaPoop /></span>
+            <span className="reaction mr-1"><FaPoop /></span>
             <span className="reaction mr-1 text-warning"><FaSadCry /></span>
             <span className="reaction mr-1 text-warning"><FaGrinSquint /></span>
             <span className="reaction mr-1 text-danger"><FaHeart /></span>
