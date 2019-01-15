995ff42283096fd4996a1b73110b255aa91827b1
Montijello <32350934+Montijello@users.noreply.github.com>
2019-01-15 14:51:33 -0800

    Create user page now logs user in and redirects to customize profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,32 +6,33 @@ import axios from 'axios'
 export default class App extends Component {
   constructor(props) {
     super(props)
-      this.state = {
-          username:'',
-          profilepic:'',
-          bio:'',
-          age:'',
-          type:'',
-          eatinghabits:'',
-          quirks:''
-                    }
-  axios.get('http://localhost:8000/accounts')
-  .then(result => {
-    let findThePet = result.data.find(ele => {return ele.username === this.props.location.state.id})
+    this.state = {
+      username: '',
+      profilepic: '',
+      bio: '',
+      age: '',
+      type: '',
+      eatinghabits: '',
+      quirks: ''
+    }
+    axios.get('http://localhost:8000/accounts')
+      .then(result => {
+        let findThePet = result.data.find(ele => { return ele.username === this.props.location.state.id })
 
-  axios.get(`http://localhost:8000/accounts/${findThePet.id}`)
-  .then(result => {const newProfile = result.data[0]
-  this.setState({
-    username:newProfile.username,
-    profilepic: newProfile.profilepic,
-    bio: newProfile.bio,
-    age:newProfile.age,
-    type:newProfile.type,
-    eatinghabits:newProfile.eatinghabits,
-    quirks:newProfile.quirks
-  })
-  })
-  })
+        axios.get(`http://localhost:8000/accounts/${findThePet.id}`)
+          .then(result => {
+            const newProfile = result.data[0]
+            this.setState({
+              username: newProfile.username,
+              profilepic: newProfile.profilepic,
+              bio: newProfile.bio,
+              age: newProfile.age,
+              type: newProfile.type,
+              eatinghabits: newProfile.eatinghabits,
+              quirks: newProfile.quirks
+            })
+          })
+      })
   }
 
   render() {
