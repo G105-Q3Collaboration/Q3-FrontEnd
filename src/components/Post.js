029ed61ceaac9c69a12b4d3cfd2ece609525d619
Katrina Agustin <katrina.agustin@gmail.com>
2019-01-15 14:08:14 -0800

    adjust alignment

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,27 +3,26 @@ import { FaThumbsUp, FaPoop, FaSadCry, FaGrinSquint, FaTrash, FaHeart } from 're
 import Moment from 'react-moment'
 
 const Post = ({ username, content, createdAt}) => {
-    return (
-      <div className="post border rounded p-0 mt-4">
-        <div className="pl-3 controls d-flex justify-content-between align-items-center">
-          <p className="thumb mt-3">{username} shared a post:<br/>
-            <small><Moment format="MMM D, YYYY" date={createdAt} /></small>
-          </p>
-          <button className="btn text-muted"><FaTrash /></button>
-        </div>
-        <p className="lead pl-3 pr-3">{content}</p>
-        {/* <img className="card-img-top" src="https://watermarked.cutcaster.com/cutcaster-photo-100067117-Cat-side-profile.jpg" alt="Cat"/> */}
-          <div className="reactions p-2">
-            <span className="reaction mr-1"><FaPoop /></span>
-            <span className="reaction mr-1 text-warning"><FaSadCry /></span>
-            <span className="reaction mr-1 text-warning"><FaGrinSquint /></span>
-            <span className="reaction mr-1 text-danger"><FaHeart /></span>
-            <span className="reaction mr-1 text-info"><FaThumbsUp /></span>&nbsp;
-            <small>400 reactions</small>
-          </div>
+  return (
+    <div className="post border rounded p-0 mt-4">
+      <div className="pl-3 controls d-flex justify-content-between align-items-center">
+        <p className="thumb mt-3">{username} shared a post:<br/>
+          <small><Moment format="MMM D, YYYY" date={createdAt} /></small>
+        </p>
+        <button className="btn text-muted"><FaTrash /></button>
       </div>
-    )
-
+      <p className="lead pl-3 pr-3">{content}</p>
+      {/* <img className="card-img-top" src="https://watermarked.cutcaster.com/cutcaster-photo-100067117-Cat-side-profile.jpg" alt="Cat"/> */}
+        <div className="reactions p-2">
+          <span className="reaction mr-1"><FaPoop /></span>
+          <span className="reaction mr-1 text-warning"><FaSadCry /></span>
+          <span className="reaction mr-1 text-warning"><FaGrinSquint /></span>
+          <span className="reaction mr-1 text-danger"><FaHeart /></span>
+          <span className="reaction mr-1 text-info"><FaThumbsUp /></span>&nbsp;
+          <small>400 reactions</small>
+        </div>
+    </div>
+  )
 }
 
 export default Post
