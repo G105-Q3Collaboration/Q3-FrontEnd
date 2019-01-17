96dda87732fd5cfdada9ac47cf7e6a17b379870f
TOBY PETERSON <TSPETERSON@)^@GMAIL.COM>
2019-01-16 16:27:31 -0800

    modified header, modified feed, added a searchbar component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import AddPost from './AddPost'
 import Post from './Post'
+import Search from './Search'
 import axios from 'axios'
 const url = 'http://localhost:8000/accounts'
 export default class Feed extends Component {
@@ -10,9 +11,13 @@ export default class Feed extends Component {
 
     this.state = {
       id: '',
-      posts : [],
+      posts:[],
+      searchedPosts:[],
       urlparams: '',
-      loggedin: ''
+      loggedin: '',
+      search:'',
+      data:[],
+      submittedSearch:false
     }
   }
 
@@ -67,12 +72,47 @@ export default class Feed extends Component {
     }
   }
 
+  handleSearchSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      const response = await axios.get(`${url}`)
+      const data = await response.data.filter(post =>
+        Object.values(post).reduce((i, b) => i || (typeof b === 'string' ?
+          b.toLowerCase().includes(this.state.search.toLowerCase()) : false), false) // need a search: '' state
+      )
+      console.log(data)
+      this.setState({
+        searchedPosts: data,
+        submittedSearch:true
+      })
+      // console.log("these are the found posts", this.state.searchedPosts)
+      return data
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name] : event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="main col-sm-8 mt-4">
+          <Search handleSearchSubmit={this.handleSearchSubmit} handleChange={this.handleChange}/>
         {
           this.state.loggedin === this.props.username &&
           <AddPost addPost={this.addPost} />
+        }
+        {
+          <div>HELLO the found friends accounts/links component will show here</div>
+
+
+          // this.state.submittedSearch ? this.state.searchedPosts.map(post =>
+          //   <Post username={post.account_id} content={post.content}/>
+          // ):null
         }
         {
           this.state.loggedin ?
---
+++ src/components/Search.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { FaSearch, FaUser, FaSignInAlt } from 'react-icons/fa'
+
+export default class Search extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+  render () {
+    return (
+      <div>
+      <form onSubmit={(event) => this.props.handleSearchSubmit(event)} className="form-inline my-lg-0 m-auto">
+                <input className="form-control mr-sm-2 searchBar"  name="search" onChange={(event) => this.props.handleChange(event)} type="search" placeholder="Search..." aria-label="Search" value={this.state.search} />
+                <button className="btn btn-success my-2 my-sm-0" type="submit"><FaSearch /></button>
+      </form>
+      </div>
+    )
+  }
+}
