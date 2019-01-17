8157dd9972f6182ea25146048e0e9406e4a09a28
Montijello <32350934+Montijello@users.noreply.github.com>
2019-01-17 15:29:29 -0800

    User image now defaults to sheep logo if not set on customize page

--- src/components/CustomizeProfile.js
+++ src/components/CustomizeProfile.js
@@ -14,10 +14,14 @@ export default class CustomizeProfile extends Component {
 
   handleCustomize = event => {
     event.preventDefault()
+    if (!this.state.profilepic) {
+      this.setState({
+        profilepic: 'http://res.cloudinary.com/squeaker/image/upload/v1547767064/sbzqwitvw02zyjwzgrld.jpg'
+      })
+    }
 
     const { displayname, age, bio, type, eatinghabits, quirks } = event.target
 
-    // under profilepic add a default url value 
     request(`/accounts/${this.props.user.id}`, 'put', {
       profilepic: this.state.profilepic,
       displayname: displayname.value,
@@ -43,14 +47,14 @@ export default class CustomizeProfile extends Component {
     acceptedFiles.forEach(file => {
       const reader = new FileReader()
       reader.onload = () => {
-        request(`/accounts/${this.props.user.id}/avatar`, 'post', { 
-          image: reader.result 
+        request(`/accounts/${this.props.user.id}/avatar`, 'post', {
+          image: reader.result
         })
-        .then(result => {
-          this.setState({profilepic: result.data.image})
-          
-        })
-        .catch(error => console.log(error))
+          .then(result => {
+            this.setState({ profilepic: result.data.image })
+
+          })
+          .catch(error => console.log(error))
       }
       reader.onabort = () => console.log('file reading was aborted')
       reader.onerror = () => console.log('file reading has failed')
