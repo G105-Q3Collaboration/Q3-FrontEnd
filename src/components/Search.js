51bfb6826f7c7657b5123a2ccc9a3376dad73e4c
Katrina Agustin <katrina.agustin@gmail.com>
2019-01-17 14:18:43 -0800

    remove search button because we're cool

--- src/components/Search.js
+++ src/components/Search.js
@@ -12,7 +12,6 @@ export default class Search extends Component {
       <div className="search p-0 mb-3">
         <form onSubmit={(event) => this.props.handleSearchSubmit(event)} className="d-flex m-auto">
           <input className="form-control mr-2 searchBar"  name="search" onChange={(event) => this.props.handleChange(event)} type="search" placeholder="Search..." aria-label="Search for your friends..." value={this.state.search} />
-          <button className="btn btn-success my-sm-0" type="submit"><FaSearch /></button>
         </form>
       </div>
     )
